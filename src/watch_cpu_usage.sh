4e4e963594a569a937db44d0a444ba1e0f8521c7
zhonghuashu <77599567@qq.com>
2024-09-06 10:17:08 +0800

    Watch cpu usage

---
+++ src/watch_cpu_usage.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+CPU_QUOTA=40
+watchCpuUsage()
+{
+    cpuTotal="$(./vmstat 1 2|tail -1|awk '{print 100-$15}')"
+
+    if [ "$cpuTotal" -gt $CPU_QUOTA ]; then
+        now=$(date +'%Y-%m-%d %H:%M:%S')
+        echo "$now: CPU usage is $cpuTotal%"
+        return 1
+    else
+        return 0
+    fi
+}
+
+echo "Watch if CPU usage < $CPU_QUOTA%"
+while true; do
+    watchCpuUsage
+    sleep 1
+done
+
+
+
